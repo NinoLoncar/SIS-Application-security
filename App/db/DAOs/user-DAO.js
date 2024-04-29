a1f117837953590b24feb841dcd08d132d9c6a2e
karlo-milos <karlomilos7@gmail.com>
2024-04-29 21:45:09 +0200

    Create DAO methods for adding and sending funds

--- App/db/DAOs/user-DAO.js
+++ App/db/DAOs/user-DAO.js
@@ -54,82 +54,20 @@ class UserDAO {
 		return result;
 	}
 
-	dajSveKorisnike = async function () {
-		this.baza.spojiSeNaBazu();
-		let sql = "SELECT * FROM korisnik;";
-		var podaci = await this.baza.izvrsiUpit(sql, []);
-		this.baza.zatvoriVezu();
-		return podaci;
-	};
+	addFunds = async function (funds, receiverId) {
+		this.db.openConnection();
+		let sql = "UPDATE users SET balance = balance + ? WHERE id =?";
+		var result = await this.db.runQuery(sql, [funds, receiverId]);
+		this.db.closeConnection();
+		return result;
+	}
 
-	dajKorisnikaPoImenu = async function (korime) {
-		this.baza.spojiSeNaBazu();
-		let sql = "SELECT * FROM korisnik WHERE korisnicko_ime=?;";
-		var podaci = await this.baza.izvrsiUpit(sql, [korime]);
-		this.baza.zatvoriVezu();
-		if (podaci.length == 1) return podaci[0];
-		else return null;
-	};
-
-	dajKorisnikaPoImenuIliMailu = async function (korime, mail) {
-		console.log("dajKorisnika " + korime + " " + mail);
-		this.baza.spojiSeNaBazu();
-		let sql = "SELECT * FROM korisnik WHERE korisnicko_ime=? OR email=?;";
-		var podaci = await this.baza.izvrsiUpit(sql, [korime, mail]);
-		this.baza.zatvoriVezu();
-		if (podaci.length != 0) return podaci;
-		else return null;
-	};
-
-	dodajKorisnika = async function (korisnik) {
-		console.log("Dodaj korisnika: " + JSON.stringify(korisnik));
-		this.baza.spojiSeNaBazu();
-		let sql = `INSERT INTO korisnik (korisnicka_uloga_id, email, korisnicko_ime, lozinka, ime, prezime, telefonski_broj, adresa, postanski_broj, datum_registracije) VALUES (?,?,?,?,?,?,?,?,?,?)`;
-		let podaci = [
-			korisnik.korisnicka_uloga_id,
-			korisnik.email,
-			korisnik.korisnicko_ime,
-			korisnik.lozinka,
-			korisnik.ime,
-			korisnik.prezime,
-			korisnik.telefonski_broj,
-			korisnik.adresa,
-			korisnik.postanski_broj,
-			korisnik.datum_registracije,
-		];
-		await this.baza.izvrsiUpit(sql, podaci);
-		this.baza.zatvoriVezu();
-		return true;
-	};
-
-	obrisiKorisnika = async function (korime) {
-		console.log("Brisem korisnika iz baze: " + korime);
-		this.baza.spojiSeNaBazu();
-		let sql = "DELETE FROM korisnik WHERE korisnicko_ime=?";
-		await this.baza.izvrsiUpit(sql, [korime]);
-		this.baza.zatvoriVezu();
-		return true;
-	};
-
-	azurirajKorisnika = async function (korime, korisnik) {
-		this.baza.spojiSeNaBazu();
-		let sql = `UPDATE korisnik SET ime=?, prezime=?, lozinka=?, adresa=?, postanski_broj=?, telefonski_broj=? WHERE korisnicko_ime=?`;
-		let podaci = [
-			korisnik.ime,
-			korisnik.prezime,
-			korisnik.lozinka,
-			korisnik.adresa,
-			korisnik.postanski_broj,
-			korisnik.telefonski_broj,
-			korime,
-		];
-
-		console.log(korisnik);
-		console.log("Azuriram korisnika");
-		await this.baza.izvrsiUpit(sql, podaci);
-		this.baza.zatvoriVezu();
-		return true;
-	};
+	sendFunds = async function (senderId, receiverId, funds) {
+		this.db.openConnection();
+		let sql = 'UPDATE users SET balance = CASE WHEN id = ? THEN balance + ? WHEN id = ? THEN balance - ? ELSE balance END WHERE id IN (?, ?);';
+		var result = await this.db.runQuery(sql, [receiverId, funds, senderId, funds, receiverId, senderId]);
+		this.db.closeConnection();
+		return result;
+	}
 }
-
 module.exports = UserDAO;
