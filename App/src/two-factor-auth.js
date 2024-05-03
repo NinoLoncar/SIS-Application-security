e0bd7230d870640337dd06ebfba3ab76d08debb3
karlo-milos <karlomilos7@gmail.com>
2024-05-03 20:20:22 +0200

    Add new attributes to user

--- App/src/two-factor-auth.js
+++ App/src/two-factor-auth.js
@@ -1,5 +1,24 @@
+const speakeasy = require("speakeasy");
+const qrcode = require("qrcode");
+
 exports.generateQRCode = async function (req, res) {
     const secret = speakeasy.generateSecret({ name: 'SiS projekt FOI IPS' });
-    //spremi secret
-    //qrcode.toData
+    //spremi secret u bazu podataka
+    qrcode.toDataURL(secret.otpauth_url, (err, QRUrl) => {
+        res.send(QRUrl);
+    });
+}
+
+exports.generateQRCode = async function (req, res) {
+    const userToken = req.body.token;
+    const verified = speakeasy.totp.verify({
+        secret: 123, //TU IDE POVUČENI KOD IZ BAZE
+        encoding: 'ascii',
+        token: userToken,
+    });
+    if (verified) {
+        res.status(200).send(true);
+    } else {
+        res.status(401).send(false);
+    }
 }
