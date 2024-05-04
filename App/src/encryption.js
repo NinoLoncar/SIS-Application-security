eec26fd9afe7b1947dda658dac4dacb578f461de
NinoLoncar <nino.loncar16@gmail.com>
2024-05-04 15:34:17 +0200

    Small fix

--- App/src/encryption.js
+++ App/src/encryption.js
@@ -1,12 +1,11 @@
 const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 
-exports.encryptSha1 = function (text) {
+exports.hashSha1 = function (text) {
 	const sha1 = crypto.createHash("sha1");
 	sha1.write(text);
 	var output = sha1.digest("hex");
 	sha1.end();
-	console.log(output);
 	return output;
 };
 
